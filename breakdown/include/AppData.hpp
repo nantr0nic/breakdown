ad1ec49665bd92edf5a0f9fff936f47bf6c8434a
nantr0nic <andy15givingup@gmail.com>
2025-12-26 14:41:16 -0800

    Moving some AppContext members into new AppData file

---
+++ breakdown/include/AppData.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Audio.hpp>
+
+#include <list>
+
+struct AppSettings
+{
+    // Audio settings
+    bool muteMusic{ false };
+    bool muteSFX{ false };
+    float volumeMusic{ 100.0f };
+    float volumeSFX{ 100.0f };
+    
+    // Resolution target settings
+    float targetWidth{ 1280.0f };
+    float targetHeight{ 720.0f };
+};
+
+struct AppData
+{
+    // Game level data
+    bool levelStarted{ false };
+    int levelNumber{ 1 };
+    int totalLevels{ 1 };
+    
+    // Game audio storage: holds sounds while they are playing
+    std::list<sf::Sound> activeSounds;
+};
